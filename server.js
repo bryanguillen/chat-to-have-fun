997932d16b504dc34518c9413da6059f820d37ef
Bryan <artificialknowledge217@gmail.com>
2017-04-07 15:21:16 -0400

    add dependencies

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 // I require express as a dependency, express allows me to handle requests and responses
 var express = require('express');
+//other dependencies
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
 // I create an express application
 var app = express();
+//HTTP log layer
+app.use(morgan('common'));
 
 // I say to my express application that I want my static content to be in the folder pages
 app.use(express.static('pages'));
