8bf1701ca9c8137128321a6d5d58a16324245512
Bryan <artificialknowledge217@gmail.com>
2017-04-07 15:48:22 -0400

    add get route

--- chatRouter.js
+++ chatRouter.js
@@ -3,5 +3,13 @@ const router = express.Router();
 
 //more dependencies
 const bodyParser = require('body-parser');
-const {Chat} = require('./model');
+const Chat = require('./model');
 
+router.get('/', (req, res) => {
+	let messages = Chat.render();
+	res.status(200).send(messages);
+});
+
+
+
+module.exports = router
